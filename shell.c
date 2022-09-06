1fd157bbbcc269edd6f7b91efcfaf0f6de16fb80
jamiu <kabirm474@gmail.com>
2022-09-06 10:15:03 -0700

    shell

--- shell.c
+++ shell.c
@@ -142,3 +142,141 @@ void execArgsPiped(char** parsed, char** parsedpipe)
  * function for finding pipe
  * function for parsing command words
  */
+void openHelp()
+{
+	puts("\n***WELCOME TO MY SHELL HELP***"
+			"\nCopyright @ rukyjay"
+			"\n-Use the shell at your own risk..."
+			"\nList of Commands supported:"
+			"\n>cd"
+			"\n>ls"
+			"\n>exit"
+			"\n>all other general commands available in UNIX shell"
+			"\n>pipe handling"
+			"\n>improper space handling");
+	return;
+}
+
+int ownCmdHandler(char** parsed)
+{
+	int NoOfOwnCmds = 4, i, switchOwnArg = 0;
+	char* ListOfOwnCmds[NoOfOwnCmds];
+	char* username;
+
+	ListOfOwnCmds[0] = "exit";
+	ListOfOwnCmds[1] = "cd";
+	ListOfOwnCmds[2] = "help";
+	ListOfOwnCmds[3] = "hello";
+
+	for (i = 0; i < NoOfOwnCmds; i++)
+	{
+		if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0)
+		{
+			switchOwnArg = i + 1;
+			break;
+		}
+	}
+	switch (switchOwnArg)
+	{
+		case 1:
+			printf("\nGoodbye\n");
+			exit(0);
+		case 2:
+			chdir(parsed[1]);
+			return (1);
+		case 3:
+			openHelp();
+			return (1);
+		case 4:
+			 username = getenv("USER");
+			 printf("\nHello %s.\nMind that this is "
+					 "not a place to play around."
+					 "\nUse help to know more..\n",
+					 username);
+			 return (1);
+		default:
+			 break;
+	}
+	return (0);
+}
+/**
+ * function for finding pipe
+ * function for parsing command words
+ * print shell line
+ * take input
+ */
+int parsePipe(char* str, char** strpiped)
+{
+	int i;
+
+	for (i = 0; i < 2; i++)
+	{
+		strpiped[i] = strsep(&str, "|");
+		if (strpiped[i] == NULL)
+			break;
+	}
+	if (strpiped[1] == NULL)
+		return (0);
+	else
+	{
+		return (1);
+	}
+}
+
+
+void parseSpace(char* str, char** parsed)
+{
+	int i;
+
+	for (i = 0; i < MAXLIST; i++)
+	{
+		parsed[i] = strsep(&str, " ");
+
+		if (parsed[i] == NULL)
+			break;
+		if (strlen(parsed[i]) == 0)
+			i--;
+	}
+}
+int processString(char* str, char** parsed, char** parsedpipe)
+{
+	char* strpiped[2];
+	int piped = 0;
+
+	piped = parsePipe(str, strpiped);
+
+	if (piped)
+	{
+		parseSpace(strpiped[0], parsed);
+		parseSpace(strpiped[1], parsedpipe);
+	}
+	else
+	{
+		parseSpace(str, parsed);
+	}
+	if (ownCmdHandler(parsed))
+		return (0);
+	else
+		return (1 + piped);
+}
+int main()
+{
+	char inputString[MAXCOM], *parsedArgs[MAXLIST];
+	char* parsedArgsPiped[MAXLIST];
+	int execFlag = 0;
+	init_shell();
+
+	while (1)
+	{
+		printDir();
+		if (takeInput(inputString))
+			continue;
+		execFlag = processString(inputString,
+				parsedArgs, parsedArgsPiped);
+		if (execFlag == 1)
+			execArgs(parsedArgs);
+		if (execFlag == 2)
+			execArgsPiped(parsedArgs, parsedArgsPiped);
+	}
+	return (0);
+}
